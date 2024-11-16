8e0f744235db0832d8cd2dc392eca74ac52b7615
Grzegorz Kunc <Grzegorz.Kunc@GAINCapital.com>
2024-11-16 19:11:21 +0100

    Start creating login screen

--- ClientApp/apps/login/src/app/app.component.ts
+++ ClientApp/apps/login/src/app/app.component.ts
@@ -9,18 +9,27 @@ import {
   HlmCardHeaderDirective,
   HlmCardTitleDirective,
 } from '@spartan-ng/ui-card-helm';
+import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
+import { HlmFormFieldModule } from '@spartan-ng/ui-formfield-helm';
+
+import { HlmSeparatorDirective } from '@spartan-ng/ui-separator-helm';
+import { BrnSeparatorComponent } from '@spartan-ng/ui-separator-brain';
 
 @Component({
   standalone: true,
   imports: [
     HlmButtonDirective,   
+    HlmInputDirective,
+    HlmFormFieldModule,
     HlmCardContentDirective,
     HlmCardDescriptionDirective,
     HlmCardDirective,
     HlmCardFooterDirective,
     HlmCardHeaderDirective,
     HlmCardTitleDirective, 
-    RouterModule
+    RouterModule,
+    HlmSeparatorDirective,
+    BrnSeparatorComponent
   ],
   selector: 'app-root',
   templateUrl: './app.component.html',
